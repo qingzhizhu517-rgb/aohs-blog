a2962308352242653d656e2ecfd59706ce7b83a7
qingzhizhu517-rgb <qingzhizhu517@gmail.com>
2026-05-23 12:17:21 +0800

    feat(cms): update projects database via visual console

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,9 @@ export const projects = [
     "id": "project-1779509265092",
     "name": "image-downloader",
     "description": "一个简单的 GUI 工具，从 Unsplash 批量下载高清图片。输入名称和文件名，点下载，完事。",
-    "tech": [],
+    "tech": [
+      "Python"
+    ],
     "stars": 1,
     "forks": 0,
     "link": "https://github.com/qingzhizhu517-rgb/image-downloader.git",
