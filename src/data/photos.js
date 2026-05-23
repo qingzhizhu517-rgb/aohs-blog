d35e2202c7185350251cb2109d4630e00b273dc3
qingzhizhu517-rgb <qingzhizhu517@gmail.com>
2026-05-23 12:32:23 +0800

    feat(cms): update photos database via visual console

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,12 +1,1 @@
-export const photos = [
-  {
-    "id": "photo-1779509848163",
-    "title": "第一次测试一下",
-    "category": "life",
-    "image": "https://ahos-java-ai.oss-cn-beijing.aliyuncs.com/1.jpg",
-    "description": "我还记得那是种下你的第一天...",
-    "aspectRatio": "aspect-landscape",
-    "date": "2026-05-23",
-    "likes": 0
-  }
-];
+export const photos = [];
