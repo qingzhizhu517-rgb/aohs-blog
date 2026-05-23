ffac2545b9c98ab96f790057e7af703ed11cea10
qingzhizhu517-rgb <qingzhizhu517@gmail.com>
2026-05-23 12:39:50 +0800

    feat(cms): update photos database via visual console

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,1 +1,12 @@
-export const photos = [];
+export const photos = [
+  {
+    "id": "photo-1779511054161",
+    "title": "第一次博客发布...",
+    "category": "life",
+    "image": "uploads/upload_1779511176054_4.jpg",
+    "description": "还记得第一次种下你的样子...",
+    "aspectRatio": "aspect-landscape",
+    "date": "2026-05-23",
+    "likes": 0
+  }
+];
